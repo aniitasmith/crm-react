5a234bf90925b7a926f2f8232695efe003b30689
Ana <ana.smith@itglobers.com>
2023-08-02 10:28:04 -0400

    first commit

---
+++ src/pages/Index.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react-refresh/only-export-components */
+import { useLoaderData } from 'react-router-dom'
+import Client from '../components/Client';
+
+export function loader() {
+  const clients = [
+    {
+        id: 1, name: 'Juan', phone: 102013313, email: 'juan@juan.com', company: 'Codigo Con Juan'
+    },
+    {
+        id: 2, name: 'Karen', phone: 138198313, email: 'karen@juan.com', company: 'Codigo Con Juan'
+    },
+    {
+        id: 3, name: 'Josue', phone: 31983913, email: 'josue@juan.com', company: 'Codigo Con Juan'
+    },
+    {
+        id: 4, name: 'Miguel', phone: 319381983, email: 'miguel@juan.com', company: 'Codigo Con Juan'
+    },
+    {
+        id: 5, name: 'Pedro', phone: 1398198938, email: 'pedro@juan.com', company: 'Codigo Con Juan'
+    },
+];
+  return clients
+}
+
+function Index() {
+
+  const clients= useLoaderData()
+
+  return (
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Clients</h1>
+      <p className='mt-3'>Manage your clients</p>
+
+      {clients.length ? (
+        <table className='w-full bg-white shadow mt-5 table-auto'>
+          <thead className='bg-blue-800 text-white'>
+            <tr>
+              <th className='p-2'>Client</th>
+              <th className='p-2'>Contact</th>
+              <th className='p-2'>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <Client
+                  client={client}
+                  key={client.id}
+                />
+              ))}
+            </tbody>
+
+          
+        </table>
+      ) : (
+        <p className='text-center mt-10'>No customers yet</p>
+      )}
+    </>
+  )
+}
+
+export default Index
---
+++ src/pages/NewClient.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable no-control-regex */
+/* eslint-disable no-useless-escape */
+/* eslint-disable react-refresh/only-export-components */
+import { useNavigate, Form, useActionData } from 'react-router-dom'
+import Formulario from '../components/Formulario'
+import Error from '../components/Error'
+
+export async function action({request}) {
+  const formData = await request.formData()
+  const datos = Object.fromEntries(formData)
+  const email = formData.get('email')
+
+  //Validation
+  const errors = []
+  if(Object.values(datos).includes('')) {
+    errors.push('All fields are required')
+  }
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)){
+    errors.push('The email is not valid')
+  }
+
+  //Return data if there are errors
+  if(Object.keys(errors).length) {
+    return errors
+  }  
+}
+
+function NewClient() {
+
+  const errors = useActionData()
+  const navigate = useNavigate()
+  return (
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>New Client</h1>
+      <p className='mt-3'>Fill in all the fields to register a new client</p>
+      <div className='flex justify-end'>
+        <button 
+          className='bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={() => navigate(-1)}
+          >
+          return
+        </button>
+      </div>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+
+        {errors?.length && errors.map( (error, i) => <Error key={i}>{error}</Error>)}
+        <Form
+          method='POST'
+          noValidate
+        >
+          <Formulario/>
+          <input
+            type='submit'
+            className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+            value='register client'
+          />
+        </Form>
+      </div>
+
+    </>
+  )
+}
+
+export default NewClient
